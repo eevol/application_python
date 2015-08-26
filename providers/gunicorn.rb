8f5e10967a974d34123f75b6756f628ebef1824d
Jakob Drangmeister <jakob@eevol.lu>
2015-08-26 15:05:34 +0200

    fix path to gunicorn conf

--- providers/gunicorn.rb
+++ providers/gunicorn.rb
@@ -89,7 +89,7 @@ action :before_deploy do
       gunicorn_command = new_resource.virtualenv.nil? ? "gunicorn" : "#{::File.join(new_resource.virtualenv, "bin", "gunicorn")}"
       base_command = "#{gunicorn_command} #{new_resource.app_module}"
     end
-    command "#{base_command} -c #{new_resource.application.path}/shared/gunicorn_config.py"
+    command "#{base_command} -c #{new_resource.path}/shared/gunicorn_config.py"
     directory new_resource.directory.nil? ? ::File.join(new_resource.path, "current") : new_resource.directory
     autostart new_resource.autostart
     user new_resource.owner
